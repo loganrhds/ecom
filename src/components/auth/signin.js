d813fb0acf6bace79b63a3e6d10e654f68829a0f
loganrhds <loganrhds@gmail.com>
2020-08-08 00:24:10 -0600

    added signUpForm

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -11,7 +11,7 @@ export default class SignIn extends Component {
   render() {
     return (
       <div className="sign-in">
-        <PageTitle className="sing-in__page-title" title="Login" />
+        <PageTitle className="sign-in__page-title" title="Login" />
         <SignInForm onSubmit={this.onSubmit} className="sign-in__form" />
       </div>
     );
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 
+import SignUpForm from "./signUpForm";
+import PageTitle from "../pageTitle";
+
 export default class SignUp extends Component {
+  onSubmit = (fields) => {
+    console.log(fields);
+  };
+
   render() {
-    return <div className="sign-up">sign upp</div>;
+    return (
+      <div className="sign-up">
+        <PageTitle className="sign-up__page-title" title="Register" />
+        <SignUpForm onSubmit={this.onSubmit} className="sign-up__form" />
+      </div>
+    );
   }
 }
