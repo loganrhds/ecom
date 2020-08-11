ffd659258b6fc20b4089cc7b5e85254f715886a4
loganrhds <loganrhds@gmail.com>
2020-08-10 22:18:03 -0600

    built user reducer action creator

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import { reducer as form } from "redux-form";
 
 import headerNavbar from "./headerNavbarReducer";
+import user from "./userReducer";
 
 const rootReducer = combineReducers({
   form,
   headerNavbar,
+  user,
 });
 
 export default rootReducer;
