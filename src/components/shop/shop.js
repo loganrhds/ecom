3e2ea9e1b0dd81f41ee65dc79d7e884913c1d190
loganrhds <loganrhds@gmail.com>
2020-08-19 13:56:40 -0600

    added greenpricetag and qunatity components

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import * as actions from "../../actions";
 
 import ShopSearchBar from "./shopSearchBar";
+import ShopProducts from "./shopProducts";
 
 class Shop extends Component {
   componentDidMount() {
@@ -40,14 +41,7 @@ class Shop extends Component {
         <ShopSearchBar onSubmit={this.onSubmit} className="shop__search-bar" />
         <div className="shop__products">
           {this.props.filteredProducts.map((product) => {
-            return (
-              <div key={product._id} className="shop-product">
-                <div className="shop-product__title">{product.title}</div>
-                <div className="shop-product__description">
-                  {product.description}
-                </div>
-              </div>
-            );
+            return <ShopProducts {...product} key={product._id} />;
           })}
         </div>
         {/* shop cart button */}
