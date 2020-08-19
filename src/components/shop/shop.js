a0422a96afa61341cdd1c8b6aabd147c5a8d04ce
loganrhds <loganrhds@gmail.com>
2020-08-18 22:53:09 -0600

    built shop search bar form and form field

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../../actions";
 
+import ShopSearchBar from "./shopSearchBar";
+
 class Shop extends Component {
   componentDidMount() {
     const headerLinks = [
@@ -28,10 +30,14 @@ class Shop extends Component {
     return true;
   }
 
+  onSubmit = (fields) => {
+    console.log(fields);
+  };
+
   render() {
     return (
       <div className="shop">
-        {/* shop search bar */}
+        <ShopSearchBar onSubmit={this.onSubmit} className="shop__search-bar" />
         <div className="shop__products">
           {this.props.filteredProducts.map((product) => {
             return (
